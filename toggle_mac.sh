8692cb4f9f1ea2d41777bd350d69b7cdb990f83d
richardhapb <richard.hapb@icloud.com>
2025-01-24 00:00:20 -0300

    feat: new layout test and toggle_mac script

---
+++ toggle_mac.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+read -p "Kanata Service: (s)top or (r)un?" choice
+
+if [ "$choice" = "r" ]; then
+    window_close=$(cat /tmp/kanata_window_id)
+    tmux kill-window -t $window_close
+    sudo launchctl start /Library/LaunchDaemons/com.kanata.service.plist
+elif [ "$choice" = "s" ]; then
+    sudo kill $(pgrep -f kanata)
+    window_id=$(tmux new-window -P -F "#{window_id}" -n "kanata" bash -c "while true; do sudo ~/kanata/kanata -c ~/kanata/kanata.kdb; done")
+    # create a temporary file to store the window id
+    echo $window_id > /tmp/kanata_window_id
+else
+    echo "Invalid choice"
+    break
+fi
+
