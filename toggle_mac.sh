fafc4d9e9e70b4094155baff9dc13b2f902ae2f4
richardhapb <richard.hapb@icloud.com>
2025-02-10 01:06:47 -0300

    feat: backspace in numeric layer

--- toggle_mac.sh
+++ toggle_mac.sh
@@ -5,7 +5,7 @@ read -p "Kanata Service: (s)top or (r)un?" choice
 if [ "$choice" = "r" ]; then
     window_close=$(cat /tmp/kanata_window_id)
     tmux kill-window -t $window_close
-    sudo launchctl start /Library/LaunchDaemons/com.kanata.service.plist
+    sudo launchctl load /Library/LaunchDaemons/com.kanata.service.plist
 elif [ "$choice" = "s" ]; then
     sudo kill $(pgrep -f kanata)
     window_id=$(tmux new-window -P -F "#{window_id}" -n "kanata" bash -c "while true; do sudo ~/kanata/kanata -c ~/kanata/kanata.kdb; done")
